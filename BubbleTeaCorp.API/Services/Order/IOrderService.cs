72f4fbfb44c0cdb805343723ec3b34211a1323f7
meof-coding <tranthe150186@fpt.edu.vn>
2024-02-04 19:31:14 +0700

    refactor Service directory

--- BubbleTeaCorp.API/Services/Order/IOrderService.cs
+++ BubbleTeaCorp.API/Services/Order/IOrderService.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using BubbleTeaCorp.API.Dtos;
-using BubbleTeaCorp.API.Entities;
 
 namespace BubbleTeaCorp.API.Services
 {
